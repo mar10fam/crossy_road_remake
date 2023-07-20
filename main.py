df724bc024d6134efe391abba5ae9e411b60c18b
mar10fam <mphamrtin@gmail.com>
2023-07-20 07:28:52 -0700

    added street and river

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pygame, sys
+import pygame, sys, random
 from frog import Frog
 from bus import Bus
+from street import Street
+from river import River
 from log import Log
 
 pygame.init()
@@ -23,8 +25,24 @@ GRAY = (175, 175, 175)
 BLUE = (0, 0, 175)
 
 frog = Frog()
-bus = Bus(Bus.STARTING_POSITION, 'Left')
-log = Log(Log.STARTING_POSITION, 'Right')
+
+# Street
+streets = []
+number_of_buses = 3
+street_height = 400
+for _ in range(2):
+    streets.append(Street(street_height, 'Left', random.randint(1, number_of_buses)))
+    streets.append(Street(street_height - 40, 'Right', random.randint(1, number_of_buses)))
+    street_height -= 80
+
+# River
+rivers = []
+number_of_logs = 3
+river_height = 200
+for _ in range(2):
+    rivers.append(River(river_height, 'Left', random.randint(1, number_of_logs)))
+    rivers.append(River(river_height - 30, 'Right', random.randint(1, number_of_logs)))
+    river_height -= 60
 
 while True:
 
@@ -44,21 +62,35 @@ while True:
             if event.key == pygame.K_d:  # D
                 frog.move_right()
 
-    if frog.rect.colliderect(bus.rect):
-        frog.reset_position()
-    if frog.rect.colliderect(log.rect):
-        frog.move_on_log(log)
+    for street in streets:
+        SCREEN.fill(GRAY, street.rect)
+        for bus in street.buses:
+            SCREEN.blit(bus.image, bus.rect)
+            bus.move()
+            if frog.rect.colliderect(bus.rect):
+                frog.reset_position()
 
-    bus.move()
-    log.move()
+        # Act on rivers and logs
+        frog_on_log = False
+        for river in rivers:
+            # Draw River
+            SCREEN.fill(BLUE, river.rect)
 
-    SCREEN.blit(bus.image, bus.rect)
-    SCREEN.blit(log.image, log.rect)
+            # Log
+            for log in river.logs:
+                SCREEN.blit(log.image, log.rect)
+                log.move()
+                if frog.rect.colliderect(log.rect):
+                    frog.move_on_log(log)
+                    frog_on_log = True
+
+            # Collided with River and not a Log
+            if not frog_on_log and frog.rect.colliderect(river.rect):
+                frog.reset_position()
+
     SCREEN.blit(frog.image, frog.rect)
 
     pygame.display.flip()
-
-    # This allows the user to close the window
 
 
 pygame.quit()
--- street.py
+++ street.py
@@ -1,16 +1,14 @@
 import pygame, random
 from bus import Bus
-class Street:
-    # Define constant values
-    SIZE = (600, 40)
-    SCREEN_DIM = 600, 500
 
-    # Creates a Street object
+
+class Street:
+    SIZE = (600, 40)
+    SCREEN_DIM = 600, 600
+
     def __init__(self, street_height: int, direction: str, number_of_buses: int):
-        # Street Information
         self.rect = pygame.Rect((0, street_height), Street.SIZE)
         self.buses = []
-        # Add Buses
         self.add_buses(direction, number_of_buses, street_height + 20)
 
     def add_buses(self, direction: str, number_of_buses: int, street_height: int):
@@ -26,3 +24,4 @@ class Street:
                     dp.append(x_pos)
                     break
             self.buses.append(Bus((x_pos, street_height), direction))
+
